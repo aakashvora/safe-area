2959fc7b13c52c1b34fb7a7551eee676c559d85c
aakash.vora <aakash.vora@arhamtechnosoft.com>
2025-10-16 17:11:04 +0530

    fix: rollup ESM output for Vite/Angular

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,23 +1,23 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+
 export default {
-  input: 'dist/esm/index.js',
+  input: 'dist/esm/index.js', // Capacitor's tsc output (esm build)
+  external: ['@capacitor/core'],
+  plugins: [resolve(), commonjs()],
   output: [
+    // ✅ ESM build (for "module")
     {
       file: 'dist/plugin.js',
-      format: 'iife',
-      name: 'capacitorSafeArea',
-      globals: {
-        '@capacitor/core': 'capacitorExports',
-      },
+      format: 'es',
       sourcemap: true,
-      inlineDynamicImports: true,
     },
+    // ✅ CJS build (for "main")
     {
       file: 'dist/plugin.cjs.js',
       format: 'cjs',
+      exports: 'named',
       sourcemap: true,
-      inlineDynamicImports: true,
     },
   ],
-  external: ['@capacitor/core'],
 };
-
